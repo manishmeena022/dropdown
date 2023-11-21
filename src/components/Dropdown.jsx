b3c16fa7ffb36d8645a813ec1416d2f7b32368e8
Manish  meena <49023276+manishmeena022@users.noreply.github.com>
2023-11-21 23:26:21 +0530

    Update Dropdown.jsx

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,19 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Dropdown = () => {
-  return (
-    <div className='main-container'>
-        <div className='dropdown'>
-            <label for="dropdown-menu">Should you use a Dropdown?</label>
-            <br/>
-            <select name='dropdown-menu'>
-                <option value="" disabled selected>Select</option>
-                <option value='yes'>Yes</option>
-                <option value='no'>Probably not</option>
-            </select>
-        </div>
-    </div>
-  )
-}
+  
+  // Items array for the dropdown options
+  const items = ["Yes", "Probably Yes", "No", "Probably No", "Not Sure"];
 
-export default Dropdown
+  // State variables
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+ // Handler for mouse enter event to show dropdown
+  const handleMouseEnter = () => {
+    setIsOpen(true);
+  };
+
+  // Handler for mouse leave event to hide dropdown
+  const handleMouseLeave = () => {
+    setIsOpen(false);
+  };
+
+  // Handler for option click to select an item and close the dropdown
+  const handleOptionClick = (selectedValue) => {
+    setSelectedItem(selectedValue);
+    setIsOpen(false);
+  };
+
+  return (
+    <div className='dropdown'>
+      <h1>Should you use a Dropdown?</h1>
+      <div className='custom-dropdown' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <button className='dropdown-button'>{selectedItem || "Select" }</button>
+        {isOpen && (
+          <ul className='dropdown-list'>
+            {items.map((item, index) => (
+              <li key={index} onClick={() => handleOptionClick(item)}>
+                {item}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Dropdown;
